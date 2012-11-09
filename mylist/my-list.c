283a9d3ae457eebfd144d5b6e5932d4a92368f37
abinesh <td.abinesh@gmail.com>
2012-11-09 10:35:54 -0800

    updated my-list

--- mylist/my-list.c
+++ mylist/my-list.c
@@ -38,22 +38,40 @@ static void mylist_add(my_list *list, char* v){
     list->size++;
     list->array = new_array;
 }
-static void mylist_delete(my_list *list, char* v){
-    int new_size = list->size - 1;
-    char **new_array = (char**)malloc(sizeof(char*)*new_size);
+
+static void mylist_delete_all(my_list *list){
+    list->size = 0;
+    list->array = NULL;
+}
+
+static int mylist_contains(my_list *list, char *v){
     int i=0;
-    int detected = 0;
     for(i=0;i<list->size;i++){
         if(strcmp(list->array[i],v) == 0) {
-            detected = 1;
-            continue;
+            return 1;
         }
-        int index = i - detected;
-        new_array[index]= (char*)malloc(sizeof(char*)*strlen(list->array[i]));
-        sprintf(new_array[index],"%s",list->array[i]);
     }
-    list->size--;
-    list->array = new_array;
+    return 0;
+}
+
+static void mylist_delete(my_list *list, char* v){
+    if(mylist_contains(list,v) == 1){
+        int new_size = list->size - 1;
+        char **new_array = (char**)malloc(sizeof(char*)*new_size);
+        int i=0;
+        int detected = 0;
+        for(i=0;i<list->size;i++){
+            if(strcmp(list->array[i],v) == 0) {
+                detected = 1;
+                continue;
+            }
+            int index = i - detected;
+            new_array[index]= (char*)malloc(sizeof(char*)*strlen(list->array[i]));
+            sprintf(new_array[index],"%s",list->array[i]);
+        }
+        list->size--;
+        list->array = new_array;
+    }
 }
 
 int main (int argc, char **argv) {
@@ -69,5 +87,13 @@ int main (int argc, char **argv) {
     mylist_print(list);
     mylist_delete(list,"abc2");
     mylist_print(list);
+    mylist_delete(list,"abc23");
+    mylist_print(list);
+    printf("%d",mylist_contains(list,"abc1"));
+    printf("%d",mylist_contains(list,"abc"));
+    mylist_delete_all(list);
+    mylist_print(list);
+
     return 0;
+
 }
