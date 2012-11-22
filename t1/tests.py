3f20b3c5fcabc7285d84192b860c621b70062eb8
abinesh <td.abinesh@gmail.com>
2012-11-21 16:53:16 -0800

    assert_key_not_found created

--- t1/tests.py
+++ t1/tests.py
@@ -3,20 +3,33 @@ import telnetlib
 
 node11211 = telnetlib.Telnet("localhost", 11211)
 
+def assert_key_not_found(node, key):
+    (value, metadata) = _do_get(key, node)
+    assert value is None, "Key expected to be absent, but was found with value:%s, metadata:%s" % (value, metadata)
+    assert metadata is None, "Key expected to be absent, but was found with value:%s, metadata:%s" % (value, metadata)
+
+
 def delete_key(node, key):
     node.write("delete " + key + "\n")
-    #    str = node.expect(["DELETED", "NOT_FOUND"])
     str = node.read_until("\r\n")
     delete_message = re.split("\r\n", str)[0]
     assert delete_message == "DELETED" or delete_message == "NOT_FOUND", "Error while deleting key %s: Message from memcached: %s" % (
         key, delete_message)
+    assert_key_not_found(node, key)
+
+
+def _do_get(key, node):
+    node.write("get " + key + "\n")
+    str = node.read_until("END")
+    output_lines = re.split("\r\n", str)
+    if len(output_lines) == 1: return None, None
+    actual_metadata = output_lines[1]
+    actual_value = output_lines[2]
+    return  actual_value, actual_metadata
 
 
 def get_key(node, key, expected_value, expected_flag=0):
-    node.write("get " + key + "\n")
-    str = node.read_until("END")
-    actual_metadata = re.split("\r\n", str)[1]
-    actual_value = re.split("\r\n", str)[2]
+    ( actual_value, actual_metadata) = _do_get(key, node)
     expected_metadata = "VALUE %s %d %d" % (key, expected_flag, len(expected_value))
     assert actual_metadata == expected_metadata, "Incorrect metadata"
     assert actual_value == expected_value, "GET: Expected:%s, Actual:%s" % (expected_value, actual_value)
