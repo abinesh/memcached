ce626b467f62acbe91fbcaf0c953854fcc96f990
abinesh <td.abinesh@gmail.com>
2012-11-23 17:23:03 -0800

    updated tests.py

--- t1/tests.py
+++ t1/tests.py
@@ -2,19 +2,25 @@ import telnetlib
 from lib import insert_keys, read_keys, delete_keys
 
 
-def test_insert_read_and_delete(node, keyprefix="key"):
-    insert_keys(node, keyprefix=keyprefix, count=50)
-    read_keys(node, keyprefix=keyprefix, count=50)
-    delete_keys(node, 50)
+def test_insert_read(node, keyprefix="key", count=50):
+    insert_keys(node, keyprefix=keyprefix, count=count)
+    read_keys(node, keyprefix=keyprefix, count=count)
 
 
-def test_read(node, keyprefix="key"):
-    read_keys(node, keyprefix=keyprefix, count=50)
+def test_insert_read_and_delete(node, keyprefix="key", count=50):
+    insert_keys(node, keyprefix=keyprefix, count=count)
+    read_keys(node, keyprefix=keyprefix, count=count)
+    delete_keys(node, count, keyprefix)
 
-node11211 = telnetlib.Telnet("localhost", 11211)
+
+def test_read(node, keyprefix="key", count=50):
+    read_keys(node, keyprefix=keyprefix, count=count)
 
 print raw_input("Have you started memcached node(11211)??")
-test_insert_read_and_delete(node11211,keyprefix = "a")
+node11211 = telnetlib.Telnet("localhost", 11211)
+test_insert_read(node11211, keyprefix="a")
 
 print raw_input("Have you started added another node(11212) to the cluster??")
-test_insert_read_and_delete(node11211,keyprefix="b")
+node11212 = telnetlib.Telnet("localhost", 11212)
+test_insert_read(node11211, keyprefix="b")
+test_read(node11212, keyprefix="b")
