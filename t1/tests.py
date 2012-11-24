5d51001a68305d632128d727a4cab075f494e240
abinesh <td.abinesh@gmail.com>
2012-11-24 15:50:43 -0800

    updated tests to run for more than one topology

--- t1/tests.py
+++ t1/tests.py
@@ -17,14 +17,14 @@ def test_read(node, keyprefix="key", count=50):
     read_keys(node, keyprefix=keyprefix, count=count)
 
 
-def normal_multinode_operations_scale_up():
-    print raw_input("Have you started memcached node(11211)??")
+def normal_multinode_operations_scale_up(topology):
+    print raw_input("Have you started memcached node(11211) in topology %s ??" % topology)
     node11211 = telnetlib.Telnet("localhost", 11211)
     test_insert_read(node11211, keyprefix="A")
     test_insert_read_and_delete(node11211, keyprefix="key")
 
     print raw_input(
-        "Have you started added another node(11212) to the cluster??\nWait for split and migrate to complete")
+        "Have you started added another node(11212) in topology %s to the cluster??\nWait for split and migrate to complete" % topology)
     node11212 = telnetlib.Telnet("localhost", 11212)
     test_read(node11211, keyprefix="A")
     test_read(node11212, keyprefix="A")
@@ -35,7 +35,7 @@ def normal_multinode_operations_scale_up():
     test_insert_read_and_delete(node11212, keyprefix="key")
 
     print raw_input(
-        "Have you started added another node(11213) to the cluster??\nWait for split and migrate to complete")
+        "Have you started added another node(11213) in topology %s to the cluster??\nWait for split and migrate to complete" % topology)
     node11213 = telnetlib.Telnet("localhost", 11213)
     test_read(node11211, keyprefix="A")
     test_read(node11212, keyprefix="A")
@@ -51,12 +51,12 @@ def normal_multinode_operations_scale_up():
     test_insert_read_and_delete(node11213, keyprefix="key")
 
 
-def normal_multinode_operations_scale_down():
+def normal_multinode_operations_scale_down(topology):
     node11211 = telnetlib.Telnet("localhost", 11211)
     node11212 = telnetlib.Telnet("localhost", 11212)
 
     print raw_input(
-        "Have you removed 11213 from the cluster??\nWait for merge and migrate to complete")
+        "Have you removed 11213 in topology %s from the cluster??\nWait for merge and migrate to complete" % topology)
     test_read(node11211, keyprefix="A")
     test_read(node11212, keyprefix="A")
 
@@ -66,7 +66,7 @@ def normal_multinode_operations_scale_down():
     test_insert_read_and_delete(node11212, keyprefix="key")
 
     print raw_input(
-        "Have you removed 11212 from the cluster??\nWait for merge and migrate to complete")
+        "Have you removed 11212 in topology %s from the cluster??\nWait for merge and migrate to complete" % topology)
     test_insert_read(node11211, keyprefix="A")
     test_insert_read_and_delete(node11211, keyprefix="key")
 
@@ -74,18 +74,22 @@ def normal_multinode_operations_scale_down():
 
 
 def print_topologies():
-    print "T1: A->B, B->C, C->D"
-    print "T2: A->B, A->C, A->D"
-    print "T3: A->B, B->C, A->D"
+    print "A-11211"
+    print "B-11212"
+    print "C-11213"
+    print "T1: A->B, B->C"
+    print "T2: A->B, A->C"
     pass
 
-def begin_tests():
-    print_topologies()
-    normal_multinode_operations_scale_up()
-    print_topologies()
-    normal_multinode_operations_scale_down()
-    print_topologies()
 
-begin_tests()
+def begin_tests(topologies):
+    for t in topologies:
+        print_topologies()
+        normal_multinode_operations_scale_up(t)
+        print_topologies()
+        normal_multinode_operations_scale_down(t)
+        print_topologies()
+
+begin_tests(["T1", "T2"])
 
 
